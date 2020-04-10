0ecbd6222c36200d275698b0cec1a9805bfe5762
Mohkh <mohkh833@gmail.com>
2020-04-10 12:56:38 +0200

    DeleteDir

--- src/DeleteDir.java
+++ src/DeleteDir.java
@@ -1,16 +1,55 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class DeleteDir {
 
     private JPanel panel;
+
+
     DeleteDir(){
-        JButton button1 = new JButton("delete 1");
-        JButton button2 = new JButton("delete 2");
+        JButton deletebutton = new JButton("delete 1");
+        JTextField deletetext = new JTextField(40);
         panel = new JPanel();
-        panel.add(button1);
-        panel.add(button2);
+        panel.add(deletebutton);
+        panel.add(deletetext);
+
+
+        deletebutton.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+
+                 /*deletefielddata = deletetext.getText();
+                 System.out.println(deletefielddata);*/
+                deletedirmethod(deletetext.getText());
+
+            }
+        });
+
+
     }
     public JPanel method(){
         return panel;
     }
-}
+
+    public  void  deletedirmethod(String deletefielddata){
+       String [] command = {"rm" , "-r" , deletefielddata};
+
+        ProcessBuilder processBuilder = new ProcessBuilder(command);
+
+        processBuilder.directory(new File(System.getProperty("user.home")));
+
+        try{
+            Process process =processBuilder.start();
+
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+
+    }
+    }
+
+
