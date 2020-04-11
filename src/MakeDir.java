14bf6a7c4cbf420e73b4cc7c8f5d4b48315a9344
Bassant557 <01273154554>
2020-04-11 14:02:14 +0200

    Make Dir/File

--- src/MakeDir.java
+++ src/MakeDir.java
@@ -1,15 +1,86 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
 
-public class MakeDir {
+public class MakeDir implements ActionListener {
     private JPanel panel;
+    JButton button1;
+    JButton button2;
+    JTextField text1;
     MakeDir(){
-        JButton button1 = new JButton("Make 1");
-        JButton button2 = new JButton("Make 2");
+        button1 = new JButton("Make Dir");
+        button2 = new JButton("Make File");
+
+        JLabel l1=new JLabel("Enter the path and dir/file name :   ");
+
+        text1 =new JTextField();
+        text1 =new JTextField(25);
+
         panel = new JPanel();
+
+        panel.add(l1);
+        panel.add(text1);
         panel.add(button1);
         panel.add(button2);
+        button1.addActionListener(this);
+        button2.addActionListener(this);
+
+
+
+
     }
     public JPanel method(){
         return panel;
+    }
+
+    public  void  MakeDirector(String dir){
+        String [] command = {"mkdir" , dir};
+
+        ProcessBuilder processBuilder = new ProcessBuilder(command);
+
+        processBuilder.directory(new File(System.getProperty("user.home")));
+
+        try{
+            Process process =processBuilder.start();
+
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+
+    }
+
+    public  void  MakeFile(String file){
+        String [] command = {"touch" , file};
+
+        ProcessBuilder processBuilder = new ProcessBuilder(command);
+
+        processBuilder.directory(new File(System.getProperty("user.home")));
+
+        try{
+            Process process =processBuilder.start();
+
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource()== button1)
+        {
+            MakeDirector(text1.getText());
+            text1.setText(" ");
+        }
+        if (e.getSource()== button2)
+        {
+            MakeFile( text1.getText());
+            text1.setText(" ");
+        }
+
+
+
     }
 }
