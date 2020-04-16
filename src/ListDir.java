24bb58f3c4cc404b84f03f29ec1072abb9a9ad97
khalifa <eng.mahmoudkhalifah@gmail.com>
2020-04-16 02:15:53 +0200

    ChangeMod

--- src/ListDir.java
+++ src/ListDir.java
@@ -1,36 +1,71 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ListDir {
     private JPanel panel;
-    ListDir(){
-        JButton button1 = new JButton("List 1");
-        JButton button2 = new JButton("List 2");
-        panel = new JPanel(new GridLayout(7,2));
-        JButton btn1 = new JButton("1");
-        JButton btn2 = new JButton("2");
-        JButton btn3 = new JButton("3");
-        JButton btn4 = new JButton("4");
-        JButton btn5 = new JButton("5");
-        JButton btn6 = new JButton("6");
-        JButton btn7 = new JButton("7");
-        JButton btn8 = new JButton("7");
-        JButton btn9 = new JButton("7");
-        JButton btn10 = new JButton("7");
-        JButton btn11 = new JButton("7");
-        panel.add(btn1);
-        panel.add(btn2);
-        panel.add(btn3);
-        panel.add(btn4);
-        panel.add(btn5);
-        panel.add(btn6);
-        panel.add(btn7);
-        panel.add(btn8);
-        panel.add(btn9);
-        panel.add(btn10);
-        panel.add(btn11);
+    JButton listBtn;
+    JTextField path;
+    JTextArea list;
+    JLabel label;
+    //GridLayout layout;
+    JScrollPane scroll;
+    ProcessBuilder processBuilder;
+    private List<String> command;
+
+    ListDir() {
+        panel = new JPanel();
+        listBtn  = new JButton("List");
+        label = new JLabel("enter the path:");
+        path = new JTextField(15);
+        list = new JTextArea(18,40);
+        list.setEditable(false);
+        list.setLineWrap(false);
+        list.setWrapStyleWord(false);
+        scroll = new JScrollPane (list, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+        //list.setSize(80,40);
+        processBuilder = new ProcessBuilder();
+        processBuilder.directory(new File(System.getProperty("user.home")));
+        command = new ArrayList<>();
+        command.add("ls");
+        command.add("-l");
+        command.add("-a");
+        listBtn.addActionListener(e->{
+            command.add(path.getText());
+            processBuilder.command(command);
+            try {
+                Process process = processBuilder.start();
+                int exitCode = process.waitFor();
+                if(exitCode != 0)
+                    JOptionPane.showMessageDialog(null, "ERROR FOUND!");
+                else {
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+                    String line;
+                    while ((line = bufferedReader.readLine())!=null) {
+                        list.setText(list.getText()+line+"\n");
+                    }
+                }
+            } catch (IOException | InterruptedException ex) {
+                ex.printStackTrace();
+            }
+        });
+        //layout = new GridLayout(4,3);
+        //layout.setVgap(10);
+        //panel.setLayout(layou);
+        panel.add(label);
+        panel.add(path);
+        panel.add(listBtn);
+        panel.add(scroll);
     }
-    public JPanel method(){
+
+    public JPanel method() {
         return panel;
     }
 }
